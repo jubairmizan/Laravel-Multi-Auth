4a8eddcaae9db2636d4e02bf5434146c6f251dc6
jubairmizan <jubairmizan@gmail.com>
2018-07-21 18:19:11 +0600

    middleware and exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -47,5 +48,25 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+
+        $guard = array_get($exception->guards(), 0);
+
+        switch ($guard) {
+          case 'admin':
+            $login = 'admin.login';
+            break;
+
+          default:
+            $login = 'login';
+            break;
+        }
+        return redirect()->guest(route($login));
     }
 }
